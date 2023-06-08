f0393a552632e7085721c602f0fa608ed141e9b3
Pascal Betting <pascal.betting@gmx.de>
2023-06-08 11:53:25 +0200

    feat: allow manual applicant addition on API layer (with validation)

--- applications/backend/src/main/java/de/pascalbe/searchrequests/applicants/api/ApplicantsController.java
+++ applications/backend/src/main/java/de/pascalbe/searchrequests/applicants/api/ApplicantsController.java
@@ -1,16 +1,22 @@
 package de.pascalbe.searchrequests.applicants.api;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @Slf4j
 public class ApplicantsController {
 
     @PostMapping("/applicants")
-    public void storeManualApplicant() {
+    public ResponseEntity<?> storeManualApplicant(@Valid @RequestBody ManualApplicant manualApplicant) {
         log.info("storeManualApplicant was called!");
+        //  TO NOTE: this should be 201 CREATED, but we have no location to return yet
+        return ResponseEntity.ok().build();
     }
 
 }
--- applications/backend/test/main/java/de/pascalbe/searchrequests/applicants/api/ApplicantsControllerTest.java
+++ applications/backend/test/main/java/de/pascalbe/searchrequests/applicants/api/ApplicantsControllerTest.java
@@ -3,6 +3,7 @@ package de.pascalbe.searchrequests.applicants.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -17,36 +18,36 @@ class ApplicantsControllerTest {
     @Test
     void shouldFailToCreateApplicantWithInvalidEmail() throws Exception {
         var requestBody = "{\"email\": \"invalid-email\", \"firstName\": \"John\", \"lastName\": \"Doe\"}";
-        this.mockMvc.perform(post("/applicants", requestBody)).andExpect(status().isBadRequest());
+        this.mockMvc.perform(post("/applicants").contentType(MediaType.APPLICATION_JSON).content(requestBody)).andExpect(status().isBadRequest());
     }
 
     @Test
     void shouldFailToCreateApplicantWithoutEmail() throws Exception {
         var requestBody = "{\"firstName\": \"John\", \"lastName\": \"Doe\"}";
-        this.mockMvc.perform(post("/applicants", requestBody)).andExpect(status().isBadRequest());
+        this.mockMvc.perform(post("/applicants").contentType(MediaType.APPLICATION_JSON).content(requestBody)).andExpect(status().isBadRequest());
     }
 
     @Test
     void shouldFailToCreateApplicantWithoutFirstName() throws Exception {
         var requestBody = "{\"email\": \"john.doe@example.com\", \"lastName\": \"Doe\"}";
-        this.mockMvc.perform(post("/applicants", requestBody)).andExpect(status().isBadRequest());
+        this.mockMvc.perform(post("/applicants").contentType(MediaType.APPLICATION_JSON).content(requestBody)).andExpect(status().isBadRequest());
     }
 
     @Test
     void shouldFailToCreateApplicantWithoutLastName() throws Exception {
         var requestBody = "{\"email\": \"john.doe@example.com\", \"firstName\": \"John\"}";
-        this.mockMvc.perform(post("/applicants", requestBody)).andExpect(status().isBadRequest());
+        this.mockMvc.perform(post("/applicants").contentType(MediaType.APPLICATION_JSON).content(requestBody)).andExpect(status().isBadRequest());
     }
 
     @Test
     void shouldAllowToCreateApplicantWithoutComment() throws Exception {
         var requestBody = "{\"email\": \"john.doe@example.com\", \"firstName\": \"John\", \"lastName\": \"Doe\"}";
-        this.mockMvc.perform(post("/applicants", requestBody)).andExpect(status().isCreated());
+        this.mockMvc.perform(post("/applicants").contentType(MediaType.APPLICATION_JSON).content(requestBody)).andExpect(status().isOk());
     }
 
     @Test
     void shouldAllowToCreateApplicantWithComment() throws Exception {
         var requestBody = "{\"email\": \"john.doe@example.com\", \"firstName\": \"John\", \"lastName\": \"Doe\", \"comment\": \"I am a comment\"}";
-        this.mockMvc.perform(post("/applicants", requestBody)).andExpect(status().isCreated());
+        this.mockMvc.perform(post("/applicants").contentType(MediaType.APPLICATION_JSON).content(requestBody)).andExpect(status().isOk());
     }
 }
