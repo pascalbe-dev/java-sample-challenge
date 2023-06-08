f6781a45d813659d5f227f916281b49990386c5b
Pascal Betting <pascal.betting@gmx.de>
2023-06-08 11:19:46 +0200

    test: add test to make sure the context loads

---
+++ applications/backend/test/main/java/de/pascalbe/searchrequests/applicants/ApplicationIT.java
@@ -0,0 +1,11 @@
+package de.pascalbe.searchrequests.applicants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationIT {
+    @Test
+    public void contextLoads() {
+    }
+}
