dbcc0657bc6da52c0df94a8836ac5e17f8e23e8a
Pascal Betting <pascal.betting@gmx.de>
2023-09-29 14:02:34 +0200

    test: extend retrieve applicants test to check for a specific property only

--- applications/backend/test/main/java/de/pascalbe/searchrequests/applicants/ApplicantRetrievalIT.java
+++ applications/backend/test/main/java/de/pascalbe/searchrequests/applicants/ApplicantRetrievalIT.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.UUID;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -16,20 +18,21 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 public class ApplicantRetrievalIT {
-    private static final String APPLICANTS_ENDPOINT = "/properties/6c54590a-04d4-46e6-b383-d1bc8be8e530/applicants";
+    private static final UUID SAMPLE_PROPERTY_ID = UUID.fromString("6c54590a-04d4-46e6-b383-d1bc8be8e530");
     private static final String VALID_REQUEST_BODY = "{\"email\": \"john.doe@example.com\", \"firstName\": \"John\", \"lastName\": \"Doe\", \"comment\": \"I am a comment\", \"salutation\": \"MRS\"}";
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    void shouldBeAbleToRetrieveAllApplicants() throws Exception {
-        this.saveApplicant("Rick");
-        this.saveApplicant("Morty");
-        this.saveApplicant("Summer");
+    void shouldBeAbleToRetrieveAllApplicantsForOneProperty() throws Exception {
+        this.saveApplicant("Rick", SAMPLE_PROPERTY_ID);
+        this.saveApplicant("Morty", SAMPLE_PROPERTY_ID);
+        this.saveApplicant("Summer", SAMPLE_PROPERTY_ID);
+        this.saveApplicant("Greg", UUID.randomUUID());
 
 
-        mockMvc.perform(get(APPLICANTS_ENDPOINT))
+        mockMvc.perform(get(getApplicantsEndpoint(SAMPLE_PROPERTY_ID)))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.length()").value(3))
                 .andExpect(jsonPath("$[0].firstName").value("Rick"))
@@ -37,11 +40,15 @@ public class ApplicantRetrievalIT {
                 .andExpect(jsonPath("$[2].firstName").value("Summer"));
     }
 
-    private void saveApplicant(String name) throws Exception {
+    private void saveApplicant(String name, UUID propertyId) throws Exception {
         var body = VALID_REQUEST_BODY.replace("John", name);
-        mockMvc.perform(post(APPLICANTS_ENDPOINT)
+        mockMvc.perform(post(getApplicantsEndpoint(propertyId))
                         .contentType("application/json")
                         .content(body))
                 .andExpect(status().isCreated());
+    }
+
+    private String getApplicantsEndpoint(UUID propertyId) {
+        return "/properties/" + propertyId + "/applicants";
     }
 }
