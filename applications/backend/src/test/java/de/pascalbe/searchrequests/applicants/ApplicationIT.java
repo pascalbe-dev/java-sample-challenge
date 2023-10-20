8729eaed1ca9bd6b24b125466df8c980f01a6732
Pascal Betting <pascal.betting@gmx.de>
2023-10-20 08:12:53 +0200

    build: fix build by making sure, the IT tests use the test profile
    - otherwise the app tries to connect to a not necessarily existing DB

--- applications/backend/src/test/java/de/pascalbe/searchrequests/applicants/ApplicationIT.java
+++ applications/backend/src/test/java/de/pascalbe/searchrequests/applicants/ApplicationIT.java
@@ -2,8 +2,10 @@ package de.pascalbe.searchrequests.applicants;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class ApplicationIT {
     @Test
     public void contextLoads() {
